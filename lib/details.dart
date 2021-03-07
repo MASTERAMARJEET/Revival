a0d4c2742a214bfbff7a480d7010b96860448c98
Amarjeet Kumar <masteramarjeetkumar@gmail.com>
2021-03-08 01:13:53 +0530

    Modified pages to use json event data

--- lib/details.dart
+++ lib/details.dart
@@ -1,13 +1,72 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter/rendering.dart';
 
+import './event/event_model.dart';
+
 class DetailsPage extends StatelessWidget {
-  DetailsPage(this.title, {Key? key}) : super(key: key);
-
-  final String title;
-
+  final EventDetail eventDetail;
+  DetailsPage(this.eventDetail);
   @override
   Widget build(BuildContext context) {
+    Widget sectionWidget(String title, List<String> list) {
+      if (list.length == 0) {
+        return Container();
+      }
+      return Column(mainAxisSize: MainAxisSize.min, children: [
+        Container(
+          alignment: Alignment.topLeft,
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 20.0,
+            top: 8.0,
+          ),
+          child: RichText(
+            text: TextSpan(
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
+              children: list.map((e) => TextSpan(text: e + '\n\n')).toList(),
+            ),
+          ),
+        ),
+      ]);
+    }
+
+    Widget headingWidget(String name, String desc) {
+      return Column(
+        children: [
+          Text(
+            name,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            desc,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Center(
@@ -66,6 +125,30 @@ class DetailsPage extends StatelessWidget {
               ),
             ),
           ),
+          Padding(
+              padding: const EdgeInsets.only(
+                left: 20.0,
+                right: 20.0,
+                top: 20.0,
+                bottom: 45.0,
+              ),
+              child: ListView(
+                children: <Widget>[
+                  headingWidget(
+                      eventDetail.name ?? "", eventDetail.shortDesc ?? ""),
+                  sectionWidget("About:", eventDetail.about ?? []),
+                  sectionWidget("Event Details:", eventDetail.details ?? []),
+                  sectionWidget("Prizes:", [eventDetail.prize ?? ""]),
+                  sectionWidget("Judging Criteria:", eventDetail.judge ?? []),
+                  sectionWidget("Eligibility Criteria:",
+                      eventDetail.rules?.eligible ?? []),
+                  sectionWidget("Participant’s Guidelines:",
+                      eventDetail.rules?.guide ?? []),
+                  sectionWidget(
+                      "Submission details:", eventDetail.submission ?? []),
+                  sectionWidget("Event timeline:", eventDetail.timeline ?? []),
+                ],
+              )),
           Positioned(
             right: 40.0,
             bottom: 20.0,
@@ -73,9 +156,6 @@ class DetailsPage extends StatelessWidget {
               'assets/images/slashdot.png',
               height: 20.0,
             ),
-          ),
-          Center(
-            child: Text(this.title),
           ),
         ],
       ),
--- lib/events.dart
+++ lib/events.dart
@@ -2,22 +2,30 @@
 
 import 'package:flutter/material.dart';
 import './details.dart';
+import './event/event_model.dart';
+import './event/event_services.dart';
 
-class EventsPage extends StatelessWidget {
+class EventsPage extends StatefulWidget {
+  @override
+  _EventsPageState createState() => _EventsPageState();
+}
+
+class _EventsPageState extends State<EventsPage> {
+  List<EventDetail> _events = [];
+  bool _loaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    loadEventList('assets/data/event_details.json')
+        .then((value) => setState(() {
+              _events = value;
+              _loaded = true;
+            }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    final events = [
-      'ARTICULATE',
-      'MELA QUIZ',
-      'CAPTURE IT',
-      'PANDEMONIUM',
-      'CICADA',
-      'PICOMIX',
-      'LAZARUS',
-      'POETRY SLAM',
-      'MANTHAN',
-      'TRASHION',
-    ];
     return Scaffold(
       appBar: AppBar(
         title: Center(
@@ -77,43 +85,46 @@ class EventsPage extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(
-              left: 20.0,
-              right: 20.0,
-              top: 20.0,
-              bottom: 45.0,
-            ),
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                mainAxisSpacing: 20.0,
-                crossAxisSpacing: 20.0,
-                crossAxisCount: 2,
+              padding: const EdgeInsets.only(
+                left: 20.0,
+                right: 20.0,
+                top: 20.0,
+                bottom: 45.0,
               ),
-              itemCount: events.length,
-              itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DetailsPage(events[index]),
-                    ),
-                  ).then((value) => null),
-                  child: Card(
-                    color: Color.fromRGBO(122, 204, 252, 1),
-                    child: Center(
-                      child: Text(
-                        events[index],
-                        style: TextStyle(
-                            color: Colors.black87,
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.bold),
+              child: _loaded
+                  ? GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        mainAxisSpacing: 20.0,
+                        crossAxisSpacing: 20.0,
+                        crossAxisCount: 2,
                       ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
+                      itemCount: _events.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return GestureDetector(
+                          onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DetailsPage(_events[index]),
+                            ),
+                          ).then((value) => null),
+                          child: Card(
+                            color: Color.fromRGBO(122, 204, 252, 1),
+                            child: Center(
+                              child: Text(
+                                _events[index].name?.toUpperCase() ?? "NONE",
+                                style: TextStyle(
+                                    color: Colors.black87,
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    )
+                  : Center(
+                      child: new CircularProgressIndicator(),
+                    )),
           Positioned(
             right: 40.0,
             bottom: 20.0,
