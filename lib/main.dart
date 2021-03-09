25e18e1716e6c76bd69b68d7797a5718cad1ac7e
Amarjeet Kumar <masteramarjeetkumar@gmail.com>
2021-03-09 12:30:22 +0530

    Revert "Updated the App title to revival"
    This reverts commit eabbdc882e58bc20d776b8f35ebf6012477aa873.

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'REVIVAL',
+      title: 'Flutter Demo',
       home: HomePage(),
       routes: {
         '/home': (BuildContext context) => HomePage(),
