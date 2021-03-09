becc8e6e305170c35f83b1b88de9745c1a6322d6
Amarjeet Kumar <masteramarjeetkumar@gmail.com>
2021-03-09 12:36:30 +0530

    Updated App title

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Revival',
       home: HomePage(),
       routes: {
         '/home': (BuildContext context) => HomePage(),
